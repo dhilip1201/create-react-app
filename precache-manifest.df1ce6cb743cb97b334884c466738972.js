42991681bc828a2df5502ff20c598c5e88d3f9e2
dhilip1201 <dhilipbeece006@gmail.com>
2020-09-17 11:41:17 +0530

    Updates

---
+++ precache-manifest.df1ce6cb743cb97b334884c466738972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bead20d1ebf203fed0265aee9580bd8a",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "7aa614da7791870fc991",
+    "url": "/create-react-app/static/css/main.dd867c7e.chunk.css"
+  },
+  {
+    "revision": "0b78f47f7a28b70911a7",
+    "url": "/create-react-app/static/js/2.8e159031.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react-app/static/js/2.8e159031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa614da7791870fc991",
+    "url": "/create-react-app/static/js/main.3f61fb45.chunk.js"
+  },
+  {
+    "revision": "f1c4cd9033896301501f",
+    "url": "/create-react-app/static/js/runtime-main.008d03ec.js"
+  }
+]);
